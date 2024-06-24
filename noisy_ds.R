af36145f455e824787c8b56bf99dae9dbe1ce0d4
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2024-06-24 13:04:18 +0000

    add problem data

--- noisy_ds.R
+++ noisy_ds.R
@@ -18,12 +18,13 @@ cores <- detectCores() - 2L
 options(mc.cores = cores)
 
 
-dsets <- replicate(1000, 
-                   noisyDat(6, set_N = sample(c(20, 30, 40, 50, 70, 80), 1),
-                            noisefraction = 0.2,
-                            n.asf = 1L), 
+dsets <- replicate(1000,
+                   noisyDat(6, set_N = sample(c(10, 20, 30), 1),
+                            noisefraction = 0.3,
+                            n.asf = 1L),
                    simplify = F)
 targets <- mclapply(dsets, \(x) attributes(x)$target)
+dsets <- lapply(dsets, \(x) cbind(x, list(U= rbinom(nrow(x), 1, 0.5))))
 
 grab_outcomes <- function(x){
   asfs <- unlist(strsplit(x, "\\)\\*\\("))
@@ -35,49 +36,60 @@ grab_outcomes <- function(x){
 outcomes <- lapply(targets, grab_outcomes)
 
 
-mods <- mcmapply(\(x,y) frscored_cna(x=x, outcome=y)[[1]],
-                 x = dsets, y = outcomes, 
+mods <- mcmapply(\(x,y,...) frscored_cna(x=x, outcome=y, ...),
+                 x = dsets,
+                 y = outcomes,
+                 MoreArgs = list(fit.range = c(1, 0.7)),
                  SIMPLIFY = F)
 
 nnull <- lapply(mods, \(x) !is.null(x)) |> unlist()
 mods <- lapply(mods, function(y) if (!is.null(y)){
-            y[y$score >= quantile(y$score, 0.98, na.rm = T),]})
-modsc <- lapply(mods[nnull], `[[`, 2)
-correct <- vector(length = length(dsets))
-correct[nnull] <- mcmapply(\(x, y) any(is.submodel(x = x, y = y)),
-                    x = modsc, y = targets[nnull],
-                    SIMPLIFY = FALSE)
+            y[[1]][y[[1]]$score >= quantile(y[[1]]$score, 0.98, na.rm = T),]})
+# modsc <- lapply(mods[nnull], `[[`, 2)
+# correct <- vector(length = length(dsets))
+# correct[nnull] <- mcmapply(\(x, y) any(is.submodel(x = x, y = y)),
+#                     x = modsc, y = targets[nnull],
+#                     SIMPLIFY = FALSE)
 
 # correct2 <- vector(length = length(dsets))
-# 
-# correct2 <- mapply(\(x, y) if(is.null(x)) TRUE else any(is.submodel(x = x$condition, y = y)),
-#                      x = mods, y = targets,
-#                      SIMPLIFY = FALSE)
+#
+correct <- mapply(\(x, y) if(is.null(x)) TRUE else any(is.submodel(x = x$condition, y = y)),
+                     x = mods, y = targets,
+                     SIMPLIFY = FALSE)
 
-correct[!nnull] <- TRUE
+# correct[!nnull] <- TRUE
 
-pv_n <- mcmapply(\(data, out) cnaTest(d = data, 
+pv_n <- mcmapply(\(data, out) cnaTest(d = data,
                                     outcomes = out,
                                     aggregFn = min)$p_value,
                data = dsets, out = outcomes,
                SIMPLIFY = FALSE)
 
-pvals_n <- pv_n
 
-res_noise <- data.table(ffree = unlist(correct), pval = unlist(pvals_n), empty = !nnull)
+res_noise <- data.table(ffree = unlist(correct), pval = unlist(pv_n), empty = !nnull)
 
 saveRDS(res_noise, "20pnoise_1000sets.RDS")
 
-# res_noise[ffree == T & pval >= 0.95, .N] 
-# 
+res_noise[ffree == T & empty == F, .N]
+#
 # res_noise[ffree == T, pval] |> hist(breaks=100)
-# 
-# 
+#
+#
 # res_noise[pval <= 1 & ffree==T, .N] / res_noise[pval <= 1, .N]
-# (res_noise[ffree == T & pval >= 0.95, .N] + res_noise[pval < 0.95, .N]) / 400
+(res_noise[ffree == T & empty == F & pval <= 0.05, .N] + res_noise[pval > 0.05, .N]) / 1000
+
+res_noise[ffree == T & empty == F & pval <= 0.05, .N] / res_noise[pval <= 0.05, .N]
+
+res_noise[ffree == T & empty == F & pval >= 0.95, .N] / res_noise[pval >= 0.95, .N]
+
+(res_noise[ffree == T & empty == F & pval >= 0.95, .N] + res_noise[pval < 0.95, .N]) / 1000
+
+res_noise[pval == 1 & ffree == T, .N] / res_noise[pval == 1, .N]
+
+res_noise[ffree == T & empty == F & pval <= 0.05, .N]
 # res_noise[pval > 0.05 & ffree == T & empty == F, .N]
 # allr <- rbind(res_noise, res_rand[, c("ffree", "pval", "empty")])
-# 
+#
 # allr[ffree == TRUE & pval >= 0.95, .N] / allr[pval >= 0.95, .N]
-# 
+#
 # allr[ffree == F, pval] |> hist()
